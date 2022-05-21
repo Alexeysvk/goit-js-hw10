400633fbdc34e03814769805b3987aee6f76dfba
Alexeysvk <83663482+Alexeysvk@users.noreply.github.com>
2022-05-21 09:52:20 +0300

    eee

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,6 +2,10 @@ const searchParams = 'name,capital,population,flags,languages';
 const baseUrl = 'https://restcountries.com/v3.1/name';
 function fetchCountries(name) { 
     return fetch(`${baseUrl}/${name}?fields=${searchParams}`)
-    .then(response => { return response.json() })  
+    .then(response => {
+        if(!response.ok){
+            throw new Error(statusText);
+        } 
+        return response.json() })  
 };
 export default { fetchCountries };
